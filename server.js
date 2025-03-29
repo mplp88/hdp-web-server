3a8fa7d58885e95733de7499b105332fc452a45d
Martín Ponce <martinp88@gmail.com>
2025-03-28 23:50:22 -0300

    added phaser default template (pa probar)

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import Pusher from 'pusher';
 import dotenv from 'dotenv';
 import path from 'path';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 // Configurar Pusher
 const pusher = new Pusher({
